21949d6d050f73c3e71fa24085ded083e503b31e
karol <karolb@onet.pl>
2020-03-07 11:58:14 +0100

    second commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String[] args) {
+            System.out.println("ss");
+        System.out.println("zzzz");
+    }
+}
---
+++ src/main/java/Tets.java
@@ -0,0 +1,5 @@
+public class Tets {
+    public static void main(String[] args) {
+        System.out.println("Klasa test");
+    }
+}
